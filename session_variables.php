42756f03ee0ae0e567f71f6ad3889cf89bd19a9f
Luis Molina <luismateo2416@gmail.com>
2021-12-14 18:34:23 +0100

    session_variables exercise done

---
+++ session_variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    session_start();
+    $_SESSION["browser"] = $_SERVER["HTTP_USER_AGENT"];
+    $_SESSION["ip"] = $_SERVER["REMOTE_ADDR"];
+    $_SESSION["time"] = time();
+    echo "<pre>", print_r($_SESSION),  "</pre>";
+
+    ?>
+
+</body>
+
+</html>
