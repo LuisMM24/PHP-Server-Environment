4881ee6786f8bcc111f7707416306171d78a3bd6
Luis Molina <luismateo2416@gmail.com>
2021-12-15 10:48:35 +0100

    log in page created

--- session-exercise/index.php
+++ session-exercise/index.php
@@ -9,9 +9,34 @@ In case you log in or are already logged in, you must be automatically redirecte
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in</title>
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
-<body>
-</body>
+<body class="text-center">
+
+    <main class="form-signin">
+        <form>
+            <img class="mb-4" src="./assets/images/Assembler.png" alt="" width="100" height="100">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+        </form>
+    </main>
 
 </html>
